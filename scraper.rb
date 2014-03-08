746a19b4a0023e31b19ad7e493942b21419759e7
boundedrational <feli87@gmx.de>
2014-03-08 10:43:01 +0000

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -63,7 +63,8 @@ for element in states
                 long_url= "http://fullrss.net/a/http/rss.indeed.com/rss?q=&l=" + url_state + "&sort=date&start="+c.to_s()
                 puts long_url
                 scraping = Nokogiri::XML(open(long_url))
-                
+                c=c+resultsperpage
+                puts c
                 scraping.css("item").each do |result|
                   puts "try"
                   identification=result.css('link').inner_html
@@ -107,8 +108,9 @@ for element in states
                   tt[identification]<< timing
                   geo[identification]<< map
   
-                  c=c+resultsperpage
                 end
+                
+
              end
        end
 
